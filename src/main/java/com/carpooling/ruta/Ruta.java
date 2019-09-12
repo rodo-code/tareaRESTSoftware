d098356517fd9bee22d911943cd1d5cf3a9199c7
Rodolfo Catunta <rodolfo.catunta.uturunco@gmail.com>
2019-09-12 09:57:20 -0400

    Creando clase Ruta como Entidad para el tutorial

---
+++ src/main/java/com/carpooling/ruta/Ruta.java
@@ -0,0 +1,22 @@
+package com.carpooling.ruta;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Ruta {
+    private @Id @GeneratedValue Long id;
+    private String zonaInicio;
+    private String zonaFinal;
+
+    Ruta(){}
+
+    Ruta(String zonaInicio,String zonaFinal){
+        this.zonaInicio = zonaInicio;
+        this.zonaFinal = zonaFinal;
+    }
+}
