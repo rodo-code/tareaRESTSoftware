481402ae4476bb15a0740f6d4d6361ce65ccd39b
Rodolfo Catunta <rodolfo.catunta.uturunco@gmail.com>
2019-09-12 09:50:34 -0400

    Iniciando el repositorio

---
+++ src/test/java/com/carpooling/ruta/RutaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carpooling.ruta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RutaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
