c2c9871b6d569b8ee869d5452689d552fdea669b
Rodolfo Catunta <rodolfo.catunta.uturunco@gmail.com>
2019-09-12 10:41:13 -0400

    Actualizando metodo uno en RutaControlador para que devuelva tambien los links de consulta

--- src/main/java/com/carpooling/ruta/RutaControlador.java
+++ src/main/java/com/carpooling/ruta/RutaControlador.java
@@ -1,8 +1,12 @@
 package com.carpooling.ruta;
 
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 public class RutaControlador {
@@ -23,9 +27,15 @@ public class RutaControlador {
     }
 
     @GetMapping("/rutas/{id}")
-    Ruta una(@PathVariable Long id){
-        return repositorioRuta.findById(id).orElseThrow(()->new ExcepcionRutaNoEncontrada(id));
+    Resource<Ruta> uno(@PathVariable Long id){
+        Ruta ruta = repositorioRuta.findById(id).orElseThrow(()-> new ExcepcionRutaNoEncontrada(id));
+        return new Resource<>(ruta,
+                linkTo(methodOn(RutaControlador.class).uno(id)).withSelfRel(),
+                linkTo(methodOn(RutaControlador.class).todo()).withRel("rutas"));
     }
+    /*Ruta una(@PathVariable Long id){
+        return repositorioRuta.findById(id).orElseThrow(()->new ExcepcionRutaNoEncontrada(id));
+    }*/
 
     @PutMapping("/rutas/{id}")
     Ruta reemplazarRuta(@RequestBody Ruta nuevaruta,@PathVariable Long id){
